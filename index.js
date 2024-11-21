0ce18ea8c5b710098f88350e255bdd932744f37e
Harish7775 <harish77sonkusare@gmail.com>
2024-11-21 16:15:17 +0530

    add cron for call dummy api

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
+import dummyRoute from "./routes/dummy.route.js";
+import cron from "node-cron";
+import axios from "axios";
 
 dotenv.config({});
 
@@ -14,27 +17,36 @@ const app = express();
 
 // middleware
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-    origin:['http://localhost:5173','https://jainjobportal.netlify.app'],
-    credentials:true
-}
+  origin: ["http://localhost:5173", "https://jainjobportal.netlify.app"],
+  credentials: true,
+};
 
 app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 3000;
-
 
 // api's
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
+app.use("/api/v1/dummy", dummyRoute);
 
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server running at port ${PORT}`);
+});
 
-
-app.listen(PORT,()=>{
-    connectDB();
-    console.log(`Server running at port ${PORT}`);
-})
+cron.schedule("*/15 * * * *", async () => {
+  try {
+    const response = await axios.get(
+      "http://localhost:8000/api/v1/dummy/getDummy"
+    );
+    console.log("API call successful:", response.data);
+  } catch (error) {
+    console.error("Error making API call:", error.message);
+  }
+});
